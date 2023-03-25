f384b1527f15c941301a2f9956770713239e1b44
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-03-26 01:59:40 +0900

    Update some files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 set -eu
 
-cd `dirname $0`
+cd $(dirname $0)
 
 TEMPLATE_FILE="template.yml"
 STACK_BUCKET="config-custom-rules"
@@ -11,8 +13,7 @@ STACK_NAME="ConfigCustomRules"
 
 WHITELIST_USERS=","
 if [ -f ${WHITELIST_USER_FILE} ]; then
-  for user in `cat ${WHITELIST_USER_FILE} | tr -d " \t" | grep -v "^#" | sed -e "s/\([^#]*\)#.*$/\1/g"`
-  do
+  for user in $(cat ${WHITELIST_USER_FILE} | tr -d " \t" | grep -v "^#" | sed -e "s/\([^#]*\)#.*$/\1/g"); do
     if [ -n "${user}" ]; then
       WHITELIST_USERS="${user},${WHITELIST_USERS}"
     fi
@@ -20,16 +21,16 @@ if [ -f ${WHITELIST_USER_FILE} ]; then
 fi
 
 BUCKET_PREFIX=$(aws sts get-caller-identity \
-    --query "Account" \
-    --output text \
-    --region ${REGION})
+  --query "Account" \
+  --output text \
+  --region ${REGION})
 
 BUCKET_PATH="${BUCKET_PREFIX}-${STACK_BUCKET}"
-if [ -z "`aws s3 ls | grep ${BUCKET_PATH}`" ]; then
+if [ -z "$(aws s3 ls | grep ${BUCKET_PATH})" ]; then
   aws s3 mb "s3://${BUCKET_PATH}"
 fi
 
-sam build --template-file ${TEMPLATE_FILE} 
+sam build --template-file ${TEMPLATE_FILE}
 
 sam deploy \
   --region ${REGION} \
@@ -37,5 +38,5 @@ sam deploy \
   --s3-bucket ${BUCKET_PATH} \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    WhitelistUsers=${WHITELIST_USERS} \
+  WhitelistUsers=${WHITELIST_USERS} \
   --no-fail-on-empty-changeset
